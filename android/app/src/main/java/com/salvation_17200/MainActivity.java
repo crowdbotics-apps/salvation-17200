6cefec537494255228311971538c3a202d0c4776
Crowdbotics <team@crowdbotics.com>
2020-05-20 19:17:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/salvation_17200/MainActivity.java
@@ -0,0 +1,15 @@
+package com.salvation_17200;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "salvation_17200";
+    }
+}
